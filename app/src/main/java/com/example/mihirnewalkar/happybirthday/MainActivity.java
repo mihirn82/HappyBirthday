74820ace9ffeaa77a80322dd76e533e614e7b8d4
mihirn82 <mihirn82@gmail.com>
2026-06-14 22:42:58 +0530

    Packages and Library upgrades

--- app/src/main/java/com/example/mihirnewalkar/happybirthday/MainActivity.java
+++ app/src/main/java/com/example/mihirnewalkar/happybirthday/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.mihirnewalkar.happybirthday;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
